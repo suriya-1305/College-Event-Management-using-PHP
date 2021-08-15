9e87712beca83c245949c928fc08f421bcdc9a92
suriya-1305 <54209218+suriya-1305@users.noreply.github.com>
2021-08-15 16:59:17 +0530

    Add files via upload

---
+++ database/connect.php
@@ -0,0 +1,10 @@
+﻿<?php
+$servername = "localhost";
+$username = "root";
+$password = FALSE;
+$dbname = "ems";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+if(!$conn){
+	die("Connection failed: ".mysqli_connect_error);
+}
+?>
